493279dbb378b2c9fee2ce0798f24a46313876f1
Mario <mario@mario-K53TA.(none)>
2013-09-11 22:11:29 +0300

    too.py

---
+++ too.py
@@ -0,0 +1,11 @@
+print "abc".capitalize()
+str = "str.center paneb lause nende vahele"
+print str.center(41, "*")
+str = "count loendab mitu kysitud t2hte on"
+sub = "o"
+print "Selles lauses on : ", str.count(sub, 0, 41), "O'd";
+str = "See on lause";
+str = str.encode('base64','strict');
+print "kodeeritud lause: " + str;
+print "lahti-kodeeritud lause: " + str.decode('base64','strict')
+

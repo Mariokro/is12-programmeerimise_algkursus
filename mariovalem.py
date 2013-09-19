d1083478c4ec20517e40b7d9a3e004b92fa84f8f
opilane <opilane@PC215-10.(none)>
2013-09-19 08:47:18 +0300

    mariovalem.py

---
+++ mariovalem.py
@@ -0,0 +1,56 @@
+# v=abc
+A = 12.0
+B = 14.0
+C = 11.0
+V = A*B*C
+print V
+print ""
+# v=s/t
+S = 55.0
+T = 35.0
+V = S/T
+print V
+print ""
+# i=u/r
+U = 317.0
+R = 43.0
+I = U/R
+print I
+print ""
+# e=mc^2
+M = 13.0
+C = 5.0
+E = M*C*C
+print E
+print ""
+# i=e/r+m
+R = 213.0
+I1 = E/(R+M)
+print I1
+print ""
+# v=a^3
+A = 7.0
+V1 = A*A*A
+print V1
+print ""
+# s=d1*d2/2
+D1 = 32.0
+D2 = 18.0
+S1 = (D1*D2)/2
+print S1
+print ""
+# p=2*(a+b)
+B = 21.0
+P = 2*(A+B)
+print P
+print ""
+# c=f+g
+F = 10
+G = 12
+print C
+print ""
+
+
+
+
+
